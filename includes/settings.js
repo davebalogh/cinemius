96a6986d984b58d75e98b0ed4e7fa3aa8705637f
David Balogh <david@balogh.com.ar>
2012-04-27 04:42:10 +0000

    validation port

--- includes/settings.js
+++ includes/settings.js
@@ -1,7 +1,7 @@
 var settings = {
   IMDB_KEY: 'b6cdaf68778e1b0b53e6900ee748cad3',
   IMDB_URL: 'api.themoviedb.org',
-  SERVER_PORT: process.env.C9_PORT,
+  SERVER_PORT: process.env.C9_PORT || process.env.PORT,
   SERVER_HOST: '0.0.0.0'
 };
 
