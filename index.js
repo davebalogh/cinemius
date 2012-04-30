ce490b7c51cc3883dee60200b3efcee0175d0c0b
David Balogh <david@balogh.com.ar>
2012-04-30 02:11:55 +0000

    require removed

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var server = require("./server/service.js");
 var router = require("./server/router.js");
 var controller = require("./index.controller.js");
-var orm = require("./model/database.js");
 
 var handle = {};
 handle["/"] = controller.index;
